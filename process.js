a37e1ceef175bc0a00c1abc4e5bdf1fb684bde5a
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-10-01 18:29:38 +0200

    this thing is buggy as heck

--- process.js
+++ process.js
@@ -33,15 +33,22 @@ rep(" 110 ", " not ");
 rep(" 010 ", " {a} ");
 rep(" 10 ", " <- ");
 rep(" 0110 ", " {c} ");
-rep(" 11000 ", " andNN ");
+rep(" 11000 ", " nor ");
 rep(" 011 ", " {b} ");
 rep(" 0111 ", " {d} ");
 rep(" 01100 ", " {e} ");
 rep(" 01101 ", " {f} ");
 rep(" 01110 ", " {g} ");
 rep(" 01111 ", " {h} ");
-rep(" 11010 ", " nor ");
-rep(" 1110 ", " thing ");
+rep(" 011000 ", " {i} ");
+rep(" 011001 ", " {j} ");
+rep(" 011010 ", " {k} ");
+rep(" 011011 ", " {l} ");
+rep(" 011100 ", " {m} ");
+rep(" 11010 ", " andNN ");
+rep(" 1110 ", " add_carry ");
+rep(" 11001 ", " xor ");
+rep(" 1111 ", " add_carry_2 ");
 
 rep(" 00101 ", " 5 ");
 rep(" 0010110 ", " 22 ");
@@ -58,3 +65,8 @@ txt = txt.replace(/\n +/g,"\n");
 txt = txt.replace(/ +\n/g,"\n");
 console.log(txt);
 
+
+// isn't this buggy?
+// or {a} {b} <- {c} not {a} <- {d} = andNN {a} {b} {d}
+// and {a} {b} <- {c} not {a} <- {d} = nor {a} {b} {d}
+// the logic feels full of bugs...
